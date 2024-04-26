05b238a8f72ee84e80111e430da43ce07051f2cb
André Laugks <al@pb82.de>
2024-04-26 18:16:51 +0200

    Finished CacheCatalogTest

--- src/test/java/io/github/alaugks/spring/messagesource/xliff/catalog/CacheCatalogTest.java
+++ src/test/java/io/github/alaugks/spring/messagesource/xliff/catalog/CacheCatalogTest.java
@@ -1,78 +1,63 @@
 package io.github.alaugks.spring.messagesource.xliff.catalog;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import io.github.alaugks.spring.messagesource.xliff.TestUtilities;
 import java.util.Locale;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("java:S5778")
 class CacheCatalogTest {
 
     private CacheCatalog cacheCatalog;
-    private Locale locale;
 
     @BeforeEach
     void beforeEach() {
-        this.cacheCatalog = new CacheCatalog(
-            TestUtilities.getCache()
-        );
-        this.locale = Locale.forLanguageTag("en");
+
+        var cache = TestUtilities.getCache();
+        cache.put("en|messages.m_en_1", "value_m_en_1");
+        cache.put("en|messages.m_en_2", "value_m_en_2");
+        cache.put("en|domain.d_en_1", "value_d_en_1");
+        cache.put("de|messages.m_de_1", "value_m_de_1");
+        cache.put("de|messages.m_de_2", "value_m_de_2");
+        cache.put("de|domain.d_de_1", "value_d_de_1");
+
+        this.cacheCatalog = new CacheCatalog(cache);
     }
 
-//    @Test
-//    void test_getAll() {
-//        var en = Locale.forLanguageTag("en");
-//        var de = Locale.forLanguageTag("de");
-//
-//        this.cacheCatalog.put(en, "messages", "m_en_1", "value_m_en_1");
-//        this.cacheCatalog.put(en, "messages", "m_en_2", "value_m_en_2");
-//        this.cacheCatalog.put(en, "domain", "d_en_1", "value_d_en_1");
-//        this.cacheCatalog.put(de, "messages", "m_de_1", "value_m_de_1");
-//        this.cacheCatalog.put(de, "messages", "m_de_2", "value_m_de_2");
-//        this.cacheCatalog.put(de, "domain", "d_de_1", "value_d_de_1");
-//
-//        var all = this.cacheCatalog.getAll();
-//        var transEn = all.get(en.toString());
-//        var transDe = all.get(de.toString());
-//
-//        assertAll(
-//            () -> assertEquals("value_m_en_1", transEn.get("messages.m_en_1")),
-//            () -> assertEquals("value_m_en_2", transEn.get("messages.m_en_2")),
-//            () -> assertEquals("value_d_en_1", transEn.get("domain.d_en_1")),
-//            () -> assertEquals("value_m_de_1", transDe.get("messages.m_de_1")),
-//            () -> assertEquals("value_m_de_2", transDe.get("messages.m_de_2")),
-//            () -> assertEquals("value_d_de_1", transDe.get("domain.d_de_1"))
-//        );
-//    }
-//
-//    @Test
-//    void test_put_get_withDomain() {
-//        this.cacheCatalog.put(this.locale, "domain", "code", "value");
-//        assertEquals("value", this.cacheCatalog.get(this.locale, "domain.code"));
-//    }
-//
-//    @Test
-//    void test_put_get() {
-//        Locale locale = Locale.forLanguageTag("en");
-//        this.cacheCatalog.put(this.locale, "code", "value");
-//        assertEquals("value", this.cacheCatalog.get(this.locale, "code"));
-//    }
-//
-//    @Test
-//    void test_get_onNull() {
-//        this.cacheCatalog.put(this.locale, "domain", "code", "value");
-//        assertNull(this.cacheCatalog.get(this.locale, "domain.foo"));
-//    }
-//
-//    @Test
-//    void test_get_onNull_localeEmpty() {
-//        this.cacheCatalog.put(this.locale, "domain", "code", "value");
-//        assertNull(this.cacheCatalog.get(Locale.forLanguageTag(""), "domain.foo"));
-//    }
-//
-//    @Test
-//    void test_put_not_overwrite() {
-//        this.cacheCatalog.put(this.locale, "domain", "code", "value_1");
-//        this.cacheCatalog.put(this.locale, "domain", "code", "value_2");
-//        assertEquals("value_1", this.cacheCatalog.get(this.locale, "domain.code"));
-//    }
+    @Test
+    void test_getAll() {
+        var en = Locale.forLanguageTag("en");
+        var de = Locale.forLanguageTag("de");
+
+        var all = this.cacheCatalog.getAll();
+        var transEn = all.get(en.toString());
+        var transDe = all.get(de.toString());
+
+        assertEquals("value_m_en_1", transEn.get("messages.m_en_1"));
+        assertEquals("value_m_en_2", transEn.get("messages.m_en_2"));
+        assertEquals("value_d_en_1", transEn.get("domain.d_en_1"));
+        assertEquals("value_m_de_1", transDe.get("messages.m_de_1"));
+        assertEquals("value_m_de_2", transDe.get("messages.m_de_2"));
+        assertEquals("value_d_de_1", transDe.get("domain.d_de_1"));
+    }
+
+    @Test
+    void test_get() {
+        assertEquals("value_m_en_1", cacheCatalog.get(Locale.forLanguageTag("en"), "messages.m_en_1"));
+    }
+
+    @Test
+    void test_get_netExists() {
+        assertNull(cacheCatalog.get(Locale.forLanguageTag("en"), "messages.not_exists"));
+    }
+
+    @Test
+    void test_get_paramValuesEmpty() {
+        assertNull(cacheCatalog.get(Locale.forLanguageTag("en"), ""));
+        assertNull(cacheCatalog.get(Locale.forLanguageTag(""), "messages.m_en_1"));
+    }
+
 }
