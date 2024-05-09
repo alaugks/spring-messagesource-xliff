159d75580eaed7da04e791f4f1f5fdfb42ce539f
André Laugks <al@pb82.de>
2024-05-09 19:38:24 +0200

    Optimized Filename.hasLocale()

--- src/main/java/io/github/alaugks/spring/messagesource/xliff/records/Filename.java
+++ src/main/java/io/github/alaugks/spring/messagesource/xliff/records/Filename.java
@@ -7,22 +7,20 @@ public record Filename(String domain, String language, String region) {
 
     public boolean hasLocale() {
         Locale locale = locale();
-        if (locale != null) {
-            return !locale.toString().isEmpty();
-        }
-        return false;
+        return locale != null && !locale.toString().isEmpty();
     }
 
     public Locale locale() {
         try {
-            Locale.Builder localeBuilder = new Locale.Builder();
             if (language != null && !language.isEmpty()) {
+                Locale.Builder localeBuilder = new Locale.Builder();
                 localeBuilder.setLanguage(language);
                 if (region != null && !region.isEmpty()) {
                     localeBuilder.setRegion(region);
                 }
+                return localeBuilder.build();
             }
-            return localeBuilder.build();
+            return null;
         } catch (IllformedLocaleException e) {
             return null;
         }
