53c1308343c416b0f86809fb15176329a9ab4b07
André Laugks <al@pb82.de>
2024-09-01 14:21:50 +0200

    Remove cache is required

---
+++ src/test/java/io/github/alaugks/spring/messagesource/xliff/XliffMatchingResourcePatternResolverInternalCacheTest.java
@@ -0,0 +1,15 @@
+package io.github.alaugks.spring.messagesource.xliff;
+
+import java.util.Locale;
+import org.junit.jupiter.api.BeforeAll;
+
+@SuppressWarnings({"java:S2187"})
+class XliffMatchingResourcePatternResolverInternalCacheTest extends XliffMatchingResourcePatternResolverAbstract {
+
+    @BeforeAll
+    static void beforeAll() {
+        messageSource = new XliffTranslationMessageSource();
+        messageSource.setBasenamePattern("translations/*");
+        messageSource.setDefaultLocale(Locale.forLanguageTag("en"));
+    }
+}
