08a50aac83ec132d0cd677384b98ed407dea0e65
André Laugks <alaugks@gmail.com>
2026-08-24 21:30:55 +0200

    Update deprecation version in XliffResourceMessageSource
    - Change @deprecated version from 3.2.0 to 3.2.1 in Javadoc
    - Update Deprecated annotation to reflect new version

--- src/main/java/io/github/alaugks/spring/messagesource/xliff/XliffResourceMessageSource.java
+++ src/main/java/io/github/alaugks/spring/messagesource/xliff/XliffResourceMessageSource.java
@@ -21,7 +21,7 @@ public class XliffResourceMessageSource {
 	}
 
 	/**
-	 * @deprecated since 3.2.0, use {@link #builder(Locale, String)} or
+	 * @deprecated since 3.2.1, use {@link #builder(Locale, String)} or
 	 * {@link #builder(Locale, List)} instead.
 	 *
 	 * Creates a new {@link Builder} for assembling an XLIFF-backed Spring
@@ -45,7 +45,7 @@ public class XliffResourceMessageSource {
 	 *                        XLIFF files are located.
 	 * @return a new builder pre-configured with the given defaults.
 	 */
-	@Deprecated(since = "3.2.0")
+	@Deprecated(since = "3.2.1")
 	public static Builder builder(Locale defaultLocale, LocationPattern locationPattern) {
 		return new Builder(defaultLocale, locationPattern.getLocationPatterns());
 	}
