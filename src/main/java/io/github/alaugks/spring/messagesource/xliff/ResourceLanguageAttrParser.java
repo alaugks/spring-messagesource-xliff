8c95e4cdfe3e2639ef4a7e450d87ae84c38cf612
André Laugks <alaugks@gmail.com>
2026-09-15 15:19:14 +0200

    Add copyright and license header
    - Add SPDX-License-Identifier and copyright header to ResourceLanguageAttrParser class

--- src/main/java/io/github/alaugks/spring/messagesource/xliff/ResourceLanguageAttrParser.java
+++ src/main/java/io/github/alaugks/spring/messagesource/xliff/ResourceLanguageAttrParser.java
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2023 André Laugks <alaugks@gmail.com>
+
 package io.github.alaugks.spring.messagesource.xliff;
 
 import io.github.alaugks.spring.messagesource.base.records.TransFileTargetLocale;
